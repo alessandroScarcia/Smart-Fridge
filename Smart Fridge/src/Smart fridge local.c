97b21f58b7822777151c8d66fe719235ec778c07
Davide Quatela <davidequatela98@gmail.com>
2018-07-11 17:55:42 +0200

    caricamento main

---
+++ Smart Fridge/src/Smart fridge local.c
@@ -0,0 +1,21 @@
+/*
+ ============================================================================
+ Name        : Smart.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include "menu.h"
+
+
+int main(){
+
+
+		menu_principale();//richiamo il menu degli alimenti
+
+		return 0;
+
+}
